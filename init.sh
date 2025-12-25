6f244598313d6061511e7475026dfcd36d1d9821
adilamine25 <99505985+adilamine25@users.noreply.github.com>
2025-12-25 17:29:52 +0100

    Update init.sh for user and RDP installation

--- init.sh
+++ init.sh
@@ -2,13 +2,18 @@
 sudo apt update
 sudo apt install -y curl gnupg lsb-release apt-transport-https
 
+# RDP
+sudo apt install -y xrdp
+sudo systemctl enable xrdp
+sudo systemctl start xrdp
+
 # Docker
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo usermod -aG docker devopsuser
+sudo usermod -aG docker adiluser
 
 # kubectl
 curl -LO https://dl.k8s.io/release/v1.30.0/bin/linux/amd64/kubectl
@@ -36,6 +41,6 @@ helm install prometheus prometheus-community/prometheus -n monitoring --create-n
 helm install grafana grafana/grafana -n monitoring
 
 # Récupération des mots de passe
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d > /home/devopsuser/argocd_password.txt
-kubectl -n monitoring get secret grafana -o jsonpath='{.data.admin-password}' | base64 -d > /home/devopsuser/grafana_password.txt
-sudo chown devopsuser:devopsuser /home/devopsuser/*.txt
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d > /home/adiluser/argocd_password.txt
+kubectl -n monitoring get secret grafana -o jsonpath='{.data.admin-password}' | base64 -d > /home/adiluser/grafana_password.txt
+sudo chown adiluser:adiluser /home/adiluser/*.txt
