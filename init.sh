b1936442ec7c6165e2b8c8cea26042b1765afe22
adilamine25 <99505985+adilamine25@users.noreply.github.com>
2025-12-26 16:25:51 +0100

    Update Docker installation command in init.sh

--- init.sh
+++ init.sh
@@ -12,7 +12,7 @@ sudo apt install -y curl gnupg lsb-release apt-transport-https \
 # Docker
 echo "🐳 Installation de Docker..."
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
